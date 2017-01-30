a868badc303dd02d8b1fe4f228a89ad8cdee7d6e
unknown <profile2697@gmail.com>
2017-01-30 23:44:29 +0900

    [Assignment4] product of independent Bernoulli-distributed random variables

--- Assignment4/cd1.m
+++ Assignment4/cd1.m
@@ -2,6 +2,7 @@ function ret = cd1(rbm_w, visible_data)
 % <rbm_w> is a matrix of size <number of hidden units> by <number of visible units>
 % <visible_data> is a (possibly but not necessarily binary) matrix of size <number of visible units> by <number of data cases>
 % The returned value is the gradient approximation produced by CD-1. It's of the same shape as <rbm_w>.
+    visible_data = sample_bernoulli(visible_data);
     v0 = visible_data;
     
     probability_for_h0 = visible_state_to_hidden_probabilities(rbm_w, v0);
