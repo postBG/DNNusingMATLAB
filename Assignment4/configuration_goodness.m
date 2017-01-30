417346f8e40a484d76ab630020b74c34c5eeb138
unknown <profile2697@gmail.com>
2017-01-30 17:21:53 +0900

    [Assignment4] implemented Energy function

--- Assignment4/configuration_goodness.m
+++ Assignment4/configuration_goodness.m
@@ -3,5 +3,10 @@ function G = configuration_goodness(rbm_w, visible_state, hidden_state)
 % <visible_state> is a binary matrix of size <number of visible units> by <number of configurations that we're handling in parallel>.
 % <hidden_state> is a binary matrix of size <number of hidden units> by <number of configurations that we're handling in parallel>.
 % This returns a scalar: the mean over cases of the goodness (negative energy) of the described configurations.
-    error('not yet implemented');
+    num_of_configurations = size(hidden_state, 2);
+    visible_hidden_products = visible_state * hidden_state';
+    visible_hidden_weight_products = rbm_w .* visible_hidden_products';
+    
+    G = sum(sum(visible_hidden_weight_products)) / num_of_configurations;
 end
+
